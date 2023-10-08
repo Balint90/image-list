7cc0eec4c718b0ddad2db686f1a58e5ab853d3ea
Bálint Csertán <csertanb@gmail.com>
2023-10-08 23:22:41 +0200

    add onFormSubmit (with error)

--- src/component/SearchInput.js
+++ src/component/SearchInput.js
@@ -7,10 +7,15 @@ class SearchInput extends React.Component {
         this.state = { entry: '' }
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+        console.log(this.state.entry)
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form action="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <div className="ui massive icon input">
                             <input 
