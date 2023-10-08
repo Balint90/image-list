babebf1c6c39537f1c92cf7525d459f5564a2f23
Bálint Csertán <csertanb@gmail.com>
2023-10-08 23:23:06 +0200

    onFormSubmit fix

--- src/component/SearchInput.js
+++ src/component/SearchInput.js
@@ -15,7 +15,7 @@ class SearchInput extends React.Component {
     render() {
         return (
             <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
+                <form onSubmit={this.onFormSubmit.bind(this)} className="ui form">
                     <div className="field">
                         <div className="ui massive icon input">
                             <input 
